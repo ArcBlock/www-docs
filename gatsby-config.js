0a66d8b4946d1102783ea7dfb0ba039557b42446
wangshijun <wangshijun2010@gmail.com>
2019-08-10 09:28:50 +0800

    chore: make gatsby develop work with latest theme plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,10 @@ const config = require('./config');
 module.exports = {
   plugins: [
     {
+      resolve: require.resolve('@arcblock/www'),
+      options: {},
+    },
+    {
       resolve: require.resolve('@arcblock/gatsby-theme-docs'),
       options: config,
     },
