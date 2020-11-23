7a13a43952acb935cbf6ef8e6848362bf367df2b
NateRobinson <840501291@qq.com>
2020-11-23 20:55:30 +0800

    change an to abtnode and update publish.sh

--- .makefiles/publish.sh
+++ .makefiles/publish.sh
@@ -3,13 +3,14 @@ echo "publishing www docs blocklet to node server..."
 yarn
 sudo npm install -g @abtnode/cli
 npm run build
+abtnode bundle --create-release
 
 # deploy to remote ABT Node
 if [ "${ALIYUN_NODE_ENDPOINT}" != "" ]; then
-  abtnode deploy . --endpoint ${ALIYUN_NODE_ENDPOINT} --access-key ${ALIYUN_NODE_ACCESS_KEY} --access-secret ${ALIYUN_NODE_ACCESS_SECRET} --skip-hooks
+  abtnode deploy .blocklet/bundle --endpoint ${ALIYUN_NODE_ENDPOINT} --access-key ${ALIYUN_NODE_ACCESS_KEY} --access-secret ${ALIYUN_NODE_ACCESS_SECRET} --skip-hooks
   echo "deploy to ${ALIYUN_NODE_ENDPOINT} success"
 fi
 if [ "${AWS_NODE_ENDPOINT}" != "" ]; then
-  abtnode deploy . --endpoint ${AWS_NODE_ENDPOINT} --access-key ${AWS_NODE_ACCESS_KEY} --access-secret ${AWS_NODE_ACCESS_SECRET} --skip-hooks
+  abtnode deploy .blocklet/bundle --endpoint ${AWS_NODE_ENDPOINT} --access-key ${AWS_NODE_ACCESS_KEY} --access-secret ${AWS_NODE_ACCESS_SECRET} --skip-hooks
   echo "deploy to ${AWS_NODE_ENDPOINT} success"
 fi
